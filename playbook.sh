3114a07877fad24c1e141ff5761d0765c0dac2a2
Waveyyyy <waves-sec@protonmail.com>
2023-06-15 14:57:22 +0100

    Updated playbook script to allow specifying debug mode

--- playbook.sh
+++ playbook.sh
@@ -1,4 +1,9 @@
-!#/bin/bash
+#! /bin/bash
 
-# add if statement to check if debug should be enabled or not!
-ansible-playbook main.yml --ask-become-pass
+# If there is no command option supplied run normally
+if [ -z "$1" ]; then
+	ansible-playbook main.yml --ask-become-pass
+# if debug is supplied (as upper or lowercase) enable debug mode
+elif [[ $(echo "$1" | awk '{print tolower($0)}') = "debug" ]]; then
+	ANSIBLE_DEBUG=true ansible-playbook main.yml --ask-become-pass
+fi
